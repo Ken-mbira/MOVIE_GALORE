95b73088997903a93fd0aa443aa59a62d0efee42
ken <ken.mbira@student.moringaschool.com>
2021-09-21 20:15:29 +0300

    add a user table

---
+++ app/auth/v1/model/models.py
@@ -0,0 +1,16 @@
+from app import db
+
+class User(db.Model):
+    """This will define the behaviours of the user class
+
+    Args:
+        db ([type]): [description]
+    """
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(200))
+    email= db.Column(db.String(200))
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'User { self.username }'
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 
 from app import create_app,db
+from app.auth.v1.model.models import User
 
 app = create_app('development')
 manager = Manager(app)
@@ -11,7 +12,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db)
+    return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':
     manager.run()
