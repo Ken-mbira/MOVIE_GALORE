84ac2a03ab23e8e9cc3e75b9af10636c9da440d0
ken <ken.mbira@student.moringaschool.com>
2021-09-21 19:56:18 +0300

    add a connection to the local database and create a function to run the shell

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from config import config_options
+
+db = SQLAlchemy()
 
 def create_app(config_name):
     """This is the app factory
@@ -8,6 +11,8 @@ def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
+
+    db.init_app(app)
 
     from app.auth.v1 import auth
     app.register_blueprint(auth)
---
+++ manage.py
@@ -0,0 +1,17 @@
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
+
+from app import create_app,db
+
+app = create_app('development')
+manager = Manager(app)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db)
+
+if __name__ == '__main__':
+    manager.run()
