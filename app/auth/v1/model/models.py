6353f4a3c5dd7e827ea3882d3c609f7683df061f
ken <ken.mbira@student.moringaschool.com>
2021-09-22 12:25:33 +0300

    add an association table to connect a user and a genre

--- app/auth/v1/model/models.py
+++ app/auth/v1/model/models.py
@@ -1,5 +1,23 @@
 from app import db,ma
 
+preferences = db.Table('preferences',
+    db.Column('user.id', db.Integer,db.ForeignKey('users.id')),
+    db.Column('genre_id', db.Integer,db.ForeignKey('genres.id'))
+)
+
+class Genre(db.Model):
+    """This will define all the preferences available for a user to choose from
+
+    Args:
+        db ([type]): [description]
+    """
+    __tablename__ = 'genres'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return self.name
+
 class User(db.Model):
     """This will define the behaviours of the user class
 
@@ -11,23 +29,11 @@ class User(db.Model):
     username = db.Column(db.String(200))
     email= db.Column(db.String(200),unique = True,index = True)
     password = db.Column(db.String)
+    preference = db.relationship('Genre', secondary=preferences, backref=db.backref('preference', lazy = 'dynamic'))
 
     def __repr__(self):
-        return self.email 
+        return self.username 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User 
-
-
-class Genre(db.Model):
-    """This will define all the preferences available for a user to choose from
-
-    Args:
-        db ([type]): [description]
-    """
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(255))
-
-    def __repr__(self):
-        return self.name
