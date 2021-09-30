532bd627021f9e4b47135059209a6d8ccdd433d1
ken <ken.mbira@student.moringaschool.com>
2021-09-30 23:22:36 +0300

    add a user and category tables

--- app/auth/v1/model/models.py
+++ app/auth/v1/model/models.py
@@ -1,5 +1,22 @@
 from app import db,ma
 
+class Issue(db.Model):
+    """This defines all the behaviours of an issue
+
+    Args:
+        db ([type]): [description]
+    """
+    __tablename__ = 'issues'
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(200))
+    body = db.Column(db.String)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    category_id = db.Column(db.Integer,db.ForeignKey('categories.id'))
+
+
+    def read(self):
+        return self.title
+
 class User(db.Model):
     """This will define the behaviours of the user class
 
@@ -11,6 +28,7 @@ class User(db.Model):
     username = db.Column(db.String(200))
     email= db.Column(db.String(200),unique = True,index = True)
     password = db.Column(db.String)
+    issue = db.relationship('Issue', backref='user',lazy="dynamic")
 
     def __repr__(self):
         return self.username 
@@ -18,3 +36,14 @@ class User(db.Model):
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User 
+
+class Category(db.Model):
+    """This defines all the categories that an issue may fall into
+
+    Args:
+        db ([type]): [description]
+    """
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(200))
+    issue = db.relationship("Issue",backref="category", lazy="dynamic")
